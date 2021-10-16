fc4689e643bb04992c7050629b3f72592e1a5e5b
awesomerobot <kris.aubuchon@discourse.org>
2021-10-15 21:51:00 -0400

    test

--- javascripts/discourse/components/referrer-banner.js
+++ javascripts/discourse/components/referrer-banner.js
@@ -5,6 +5,7 @@ export default Component.extend({
   referrer: computed(function () {
     let searchTerms = new RegExp(settings.match_urls, "gm");
     let referrer = document.referrer;
+    console.log(referrer);
     return searchTerms.test(referrer);
   }),
 });
