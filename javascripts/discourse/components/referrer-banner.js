63a6a6eb6a301f30c12ddac399b57efb722d006e
awesomerobot <kris.aubuchon@discourse.org>
2021-10-15 22:19:59 -0400

    change outlet

--- javascripts/discourse/components/referrer-banner.js
+++ javascripts/discourse/components/referrer-banner.js
@@ -5,7 +5,6 @@ export default Component.extend({
   referrer: computed(function () {
     let searchTerms = new RegExp(settings.match_urls, "gm");
     let referrer = document.referrer;
-    console.log(referrer);
     return searchTerms.test(referrer);
   }),
 });
