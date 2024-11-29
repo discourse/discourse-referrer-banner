4d2d4dcd322db8a0fd728eee7a54a7090dd5bed6
David Taylor <david@taylorhq.com>
2024-11-29 10:52:33 +0000

    DEV: Convert to native class syntax

--- javascripts/discourse/components/referrer-banner.js
+++ javascripts/discourse/components/referrer-banner.js
@@ -1,10 +1,11 @@
 import Component from "@ember/component";
 import { computed } from "@ember/object";
 
-export default Component.extend({
-  referrer: computed(function () {
+export default class ReferrerBanner extends Component {
+  @computed
+  get referrer() {
     let searchTerms = new RegExp(settings.match_urls, "gm");
     let referrer = document.referrer;
     return searchTerms.test(referrer);
-  }),
-});
+  }
+}
